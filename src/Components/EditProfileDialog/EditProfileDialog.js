3ed464d351b946720965a3a14865403b4d587d24
Haruna Oseni <harunaoseni23@gmail.com>
2021-07-09 02:15:32 -0500

    edit profile component implemented

--- src/Components/EditProfileDialog/EditProfileDialog.js
+++ src/Components/EditProfileDialog/EditProfileDialog.js
@@ -1,24 +1,55 @@
 import React, { useState, useEffect } from "react";
-import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
 import Dialog from "@material-ui/core/Dialog";
 import { DialogActions } from "@material-ui/core";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogTitle from "@material-ui/core/DialogTitle";
-import { database } from "../../Firebase/firebase";
+import { database, storage } from "../../Firebase/firebase";
 import Avatar from "@material-ui/core/Avatar";
-import {makeStyles} from "@material-ui/core/styles";
+import { makeStyles } from "@material-ui/core/styles";
+import { Fab, Button } from "@material-ui/core";
+import AddIcon from "@material-ui/icons/Add";
+import DoneAllIcon from "@material-ui/icons/DoneAll";
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   root: {
     display: "flex",
-  }
+  },
 
   large: {
-    width: theme.spacing(7),
-    height: theme.spacing(7),
-  }
- }))
+    width: theme.spacing(10),
+    height: theme.spacing(10),
+  },
+  textField: {
+    justifyContent: "center",
+    margin: theme.spacing(1),
+    display: "none",
+  },
+
+  uploadProfilePicture: {
+    margin: theme.spacing(1),
+    color: "white",
+    backgroundColor: "rgba(43,72,158,0.3)",
+    "&:hover": {
+      backgroundColor: "whiteSmoke",
+      color: "rgba(43,72,158,0.946)",
+    },
+    marginTop: theme.spacing(3),
+  },
+
+  profileUpload: {
+    display: "flex",
+    flexDirection: "column",
+  },
+
+  doneIcon: {
+    marginLeft: theme.spacing(2),
+    color: "lightgreen",
+  },
+  profilePic__info: {
+    display: "flex",
+  },
+}));
 
 function EditProfileDialog({ handleEditProfileToggle, handleSnackbarToggle }) {
   const [open, setOpen] = useState(true);
@@ -27,6 +58,8 @@ function EditProfileDialog({ handleEditProfileToggle, handleSnackbarToggle }) {
   const [email, setEmail] = useState("");
   const [userId, setUserId] = useState("");
   const [profilePicture, setProfilePicture] = useState(null);
+  const [profilePictureFile, setProfilePictureFile] = useState(null);
+  const classes = useStyles();
 
   useEffect(() => {
     const userInfo = JSON.parse(localStorage.getItem("userDetails"));
@@ -37,13 +70,20 @@ function EditProfileDialog({ handleEditProfileToggle, handleSnackbarToggle }) {
     setProfilePicture(userInfo.photoUrl);
   }, []);
 
+  function handleProfilePicture(event) {
+    setProfilePictureFile(event.target.files[0]);
+  }
+
   function handleCloseDialogue() {
     setOpen(false);
     handleEditProfileToggle();
   }
 
-  function updateProfileDisplayName(event) {
+  function updateUserProfile(event) {
     event.preventDefault();
+    const uploadTask = storage
+      .ref(`profilePictures/${profilePictureFile.name}`)
+      .put(profilePictureFile);
     database
       .collection("users")
       .doc(userId)
@@ -54,6 +94,32 @@ function EditProfileDialog({ handleEditProfileToggle, handleSnackbarToggle }) {
         console.log(error);
       });
 
+    uploadTask.on(
+      (error) => {
+        if (error) {
+          console.log(error);
+        }
+      },
+      () => {
+        storage
+          .ref("profilePictures")
+          .child(profilePictureFile.name)
+          .getDownloadURL()
+          .then((url) => {
+            database
+              .collection("users")
+              .doc(userId)
+              .update({
+                photoUrl: url,
+              })
+              .catch((error) => {
+                console.log(error);
+              });
+          });
+      }
+    );
+
+    setProfilePictureFile(null);
     handleSnackbarToggle();
     setOpen(false);
     handleEditProfileToggle();
@@ -79,6 +145,41 @@ function EditProfileDialog({ handleEditProfileToggle, handleSnackbarToggle }) {
         <DialogTitle>Edit {`${originalName}'s`} Profile</DialogTitle>
         <DialogContent>
           <form autoComplete="off">
+            <div className={classes.root}>
+              <Avatar
+                src={profilePicture}
+                alt={originalName}
+                className={classes.large}
+              />
+              <label
+                htmlFor="profilePicture__upload"
+                className={classes.profileUpload}
+              >
+                <TextField
+                  name="profile picture upload"
+                  type="file"
+                  className={classes.textField}
+                  id="profilePicture__upload"
+                  onChange={handleProfilePicture}
+                />
+                <Fab
+                  size="small"
+                  component="span"
+                  aria-label="add"
+                  variant="extended"
+                  className={classes.uploadProfilePicture}
+                >
+                  <AddIcon /> Change photo
+                </Fab>
+                {profilePictureFile !== null ? (
+                  <div className={classes.profilePic__info}>
+                    <DoneAllIcon className={classes.doneIcon} />
+                    <h4>Click the confirm button to complete change!</h4>
+                  </div>
+                ) : null}
+              </label>
+            </div>
+
             <TextField
               id="outlined-basic"
               label="Name"
@@ -93,9 +194,6 @@ function EditProfileDialog({ handleEditProfileToggle, handleSnackbarToggle }) {
                 color: "black",
               }}
             />
-            <div>
-              <Avatar  />
-            </div>
             <TextField
               id="outlined-basic"
               label="Email"
@@ -133,7 +231,7 @@ function EditProfileDialog({ handleEditProfileToggle, handleSnackbarToggle }) {
             Cancel
           </Button>
           <Button
-            onClick={updateProfileDisplayName}
+            onClick={updateUserProfile}
             color="primary"
             variant="contained"
           >
